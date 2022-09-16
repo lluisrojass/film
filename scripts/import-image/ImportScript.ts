75a9000ba0eebbf7f1e47f0fd1fec11eb9e2ffc2
Luis Rojas <lluisrojass@gmail.com>
2022-09-15 23:23:25 -0400

    feat: Onboarding more images

--- scripts/import-image/ImportScript.ts
+++ scripts/import-image/ImportScript.ts
@@ -55,7 +55,7 @@ class ImportScript {
       target: '_blank',
       alt: 'alt',
       href: imagePath,
-      lazy: true,
+      lazy: false,
       srcSets: [
         {
           src: smThumbnailPath,
